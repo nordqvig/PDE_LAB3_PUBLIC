dff338cba5dd893dca32cfb067bff6049b34ce2a
Georgios Nordqvist <geonor@kth.se>
2024-10-17 10:30:41 +0200

    Testing solver dirichlet solver OK. Testing error_calc OK.

--- PDE_LAB3.py
+++ PDE_LAB3.py
@@ -13,6 +13,8 @@ class Solver:
      self.beta - the right boundary condition (at x=1) (dirichlet OR neumann).
      self.sol_vec_dirichlet - Attribute to hold the numerical solution obtained with dirichlet boundary conditions
      self.sol_vec_mixed - Attribute to hold the numerical solution obtained with mixed boundary conditions
+     self.error_dirichlet - Attribute to hold the infinity error as outlined in exercise 3 for dirichlet solution.
+     self.error_mixed - Attribute to hold the infinity error as outlined in exercise 3 for mixed solution.
      """
     def __init__(self, n, alpha, beta):
         self.n = n
@@ -20,8 +22,10 @@ class Solver:
         self.X_vec = np.linspace(0,1,self.n+1)
         self.alpha = alpha
         self.beta = beta
-        self.sol_vec_dirichlet = None
-        self.sol_vec_mixed = None
+        self.sol_vec_dirichlet = []
+        self.sol_vec_mixed = []
+        self.error_dirichlet = None
+        self.error_mixed = None
 
     """The function a(x). Takes in an x-value and returns a(x)."""
     def a(self, x):
@@ -33,7 +37,7 @@ class Solver:
     """The analytical solution to the PDE with dirichlet boundary conditions, sol_exact_dirichlet. Takes in an x-value 
     and returns sol_exact_dirichlet(x). """
     def sol_exact_dirichlet(self, x): # TODO
-        pass
+        return 1
 
     """The analytical solution to the PDE with mixed boundary conditions, sol_exact_mixed. Takes in an x-value and returns
     sol_exact_mixed(x). """
@@ -79,13 +83,13 @@ class Solver:
 
     """Calculates the error_inf as outlined in exercise 3). Sets the attributes self.error_dirichlet and self.error_mixed. 
     Should be called after calculating at least 1 numerical solution. """
-    def calculate_error_inf(self):
-        if self.sol_vec_dirichlet:
+    def calculate_error(self):
+        if type(self.sol_vec_dirichlet) != []:
             sol_vec_exact_dirichlet = [self.sol_exact_dirichlet(x_i) for x_i in self.X_vec] # vector holding exact solution at every gridpoint
             temp_vec = np.array(sol_vec_exact_dirichlet) - np.array(self.sol_vec_dirichlet) # vector holding error at each gridpoint
             self.error_dirichlet = max(np.abs(temp_vec))
 
-        if self.sol_vec_mixed:
+        if self.sol_vec_mixed != []:
             sol_vec_exact_mixed = [self.sol_exact_mixed(x_i) for x_i in self.X_vec]
             temp_vec = np.array(sol_vec_exact_mixed) - np.array(self.sol_vec_mixed)
             self.error_mixed = max(np.abs(temp_vec))
@@ -94,14 +98,12 @@ def main():
     pass
 
 def testing():
-    solver1 = Solver(1000, 0, 1)
-    print(solver1.n)
-    print(solver1.h)
-    print(solver1.X_vec)
-    print(solver1.alpha)
-    print(solver1.beta)
+    solver1 = Solver(3, 0, 1)
+    solver1.solve_dirichlet()
     print(solver1.sol_vec_dirichlet)
-    print(solver1.sol_vec_mixed)
+    solver1.calculate_error()
+    print(solver1.error_dirichlet)
+
 
 if __name__ == '__main__':
     testing()
